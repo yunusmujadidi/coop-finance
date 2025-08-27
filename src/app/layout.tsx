b4a49c5a102044a7cb99224ad61a062bdced32a6
yunusmujadidi <yunusmujadidi@gmail.com>
2025-08-27 21:19:05 +0700

    feat: setup auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
         <ThemeProvider
